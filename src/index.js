73d43b4c4a47ee729f00dae61985b1ba6a7cb8e1
ifarnami <farnam.faryar@gmail.com>
2024-06-28 00:10:21 +0330

    fix: fix cors

--- src/index.js
+++ src/index.js
@@ -3,22 +3,20 @@ import express from "express";
 import "./db/mongoose.js";
 import taskRoutes from "./routes/task.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(taskRoutes);
 app.use(userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port " + 3000);
 });
-
-// const main = async () => {
-//     const user = await User.findById("6330d1e9b0995a507fedcf4f")
-//     await user.populate('tasks')
-
-//     console.log(user.tasks)
-// }
-
-// main()
